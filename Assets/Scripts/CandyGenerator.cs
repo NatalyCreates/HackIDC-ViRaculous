97140888df6dc9f278262cdd50cd122365b6e562
stavhertz@gmail.com <stavhertz@gmail.com>
2016-05-06 18:18:25 +0300

    Creating many gingerbreads in random locaitons in the scene

--- Assets/Scripts/CandyGenerator.cs
+++ Assets/Scripts/CandyGenerator.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class CandyGenerator : MonoBehaviour {
 
+    public GameObject cookie;
+
 	// Use this for initialization
 	void Start () {
-	
+        GameObject ground = GameObject.FindWithTag("Ground");
+        Debug.Log(ground.ToString());
+        Transform rt = ground.transform;
+        Debug.Log(rt.ToString());
+        Debug.Log(ground.GetComponent<Renderer>().bounds.size.x);
+        Debug.Log(ground.GetComponent<Renderer>().bounds.size.z);
+
+        for (int i = 0; i < 50000; i++)
+        {
+
+                float xVal = Random.Range(0, ground.GetComponent<Renderer>().bounds.size.x) /100;
+                float zVal = Random.Range(0, ground.GetComponent<Renderer>().bounds.size.z) /100;
+
+                Instantiate(cookie, new Vector3(xVal, 2, zVal), Quaternion.Euler(270f, 0f, 0f));
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Bird")
+        {
+            Destroy(col.gameObject);
+        }
+    }
 }
